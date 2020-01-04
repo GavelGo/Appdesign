a54aad74888ae4fa1fdc2b1e4b7fa58bc427c2d6
TalwinderSingh <tarun.grewal330@gmail.com>
2020-01-04 21:29:54 +0530

    images

--- app/src/main/java/Mobile/gavelgo/View/Fragment/SettingsFragment.java
+++ app/src/main/java/Mobile/gavelgo/View/Fragment/SettingsFragment.java
@@ -16,16 +16,7 @@ public class SettingsFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,@Nullable Bundle savedInstanceState){
 
-//        homeViewModel =
-//                ViewModelProviders.of(this).get(HomeViewModel.class);
-//        View root = inflater.inflate(R.layout.fragment_home, container, false);
-//        final TextView textView = root.findViewById(R.id.text_home);
-//        homeViewModel.getText().observe(this, new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
+
         return inflater.inflate(R.layout.fragment_setting, container, false);
     }
 
